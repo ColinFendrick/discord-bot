2c73251bf2e090f0da058c172da6726afa8e0905
Colin Fendrick <colin.fendrick@gmail.com>
2020-12-17 08:50:47 -0800

    Loop through guild members

--- bot.py
+++ bot.py
@@ -1,15 +1,23 @@
 import os
-
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+	guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+
+	members = '\n - '.join([member.name for member in guild.members])
+	print(
+		f'{client.user} is connected to the following guild:\n'
+		f'{guild.name}(id: {guild.id})\n'
+		f'Guild Members:\n - {members}'
+	)
 
 client.run(TOKEN)
